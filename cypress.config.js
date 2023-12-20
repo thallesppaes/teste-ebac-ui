e16e306f7d18ffbf80bec6928ef018d8c03be11d
Thalles Augusto <thalles-paes@hotmail.com>
2023-12-20 13:34:06 -0300

    Meu primeiro commit

---
+++ cypress.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  projectId: 'cu6qdy',
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
