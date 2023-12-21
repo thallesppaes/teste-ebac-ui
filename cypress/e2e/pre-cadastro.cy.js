444aad368b86bfb6af16b89092d43c228d24c1a9
Thalles Augusto <thalles-paes@hotmail.com>
2023-12-21 14:13:39 -0300

    Alteracoes nos testes automatizados.

---
+++ cypress/e2e/pre-cadastro.cy.js
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+import { faker } from '@faker-js/faker';
+
+describe('Funcionalidade Pré cadastro', () => {
+
+  beforeEach(() => {
+    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+  });
+
+  it('Deve completar o pré cadastro com sucesso', () => {
+    let fakerNome = faker.name.firstName()
+    let fakerSobrenome = faker.name.lastName()
+    let fakerEmail = faker.internet.email(fakerNome)
+
+    cy.get('#reg_email').type(fakerEmail)
+    cy.get('#reg_password').type('!teste@teste$')
+    cy.get(':nth-child(4) > .button').click()
+
+    cy.get('.woocommerce-MyAccount-navigation-link--edit-account > a').click()
+    cy.get('#account_first_name').type(fakerNome)
+    cy.get('#account_last_name').type(fakerSobrenome)
+    cy.get('.woocommerce-Button').click()
+
+    cy.get('.woocommerce-message').should('contain', "Detalhes da conta modificados com sucesso.")
+  });
+});
