035c4bf4496c625c3a0448522dfae69c6d08fcc9
Thalles Augusto <thalles-paes@hotmail.com>
2023-12-26 13:18:13 -0300

    Atualização - Page Objects

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+import enderecoPage from "../support/page-objects/endereco.page";
+const dadosEndereco = require('../fixtures/enderecos.json')
 
 describe('Funcionlidade Endereços - Faturamento e Entrega', () => {
   beforeEach(() => {
@@ -9,6 +11,21 @@ describe('Funcionlidade Endereços - Faturamento e Entrega', () => {
   });
 
   it('Deve fazer cadastro e faturamento com sucesso', () => {
+    enderecoPage.editarEnderecoFaturamento('Junior', 'Alves', 'Vivo', 'Brasil', 'Av Joao Paulo', '200', 'Cerquilho', 'São Paulo', '02002000', '14999999999', 'ebac-teste@dominio.com')
+  });
 
+  i.only('Deve fazer cadastro e faturamento com sucesso - Usando arquivo de dados', () => {
+    enderecoPage.editarEnderecoFaturamento(
+      dadosEndereco[1].nome,
+      dadosEndereco[1].sobrenome,
+      dadosEndereco[1].empresa,
+      dadosEndereco[1].pais,
+      dadosEndereco[1].endereco,
+      dadosEndereco[1].numero,
+      dadosEndereco[1].cidade,
+      dadosEndereco[1].estado,
+      dadosEndereco[1].cep,
+      dadosEndereco[1].telefone,
+      dadosEndereco[1].email)
   });
 });
